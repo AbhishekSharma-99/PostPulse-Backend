a94fc1a86720810e79f1201e6d17bd6a64643ac8
Abhishek Sharma <abhisheksh3397@gmail.com>
2026-05-08 10:33:17 +0000

    chore: remove OpenAPI annotations from PostPulseApplication

--- src/main/java/com/postpulse/PostPulseApplication.java
+++ src/main/java/com/postpulse/PostPulseApplication.java
@@ -1,34 +1,9 @@
 package com.postpulse;
 
-import io.swagger.v3.oas.annotations.ExternalDocumentation;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-        info = @Info(
-                title = "PostPulse REST API",
-                version = "1.0.0",
-                description = "A blog platform REST API built with Spring Boot, Spring Security and JWT authentication",
-                contact = @Contact(
-                        name = "Abhishek Sharma",
-                        email = "abhisheksh3397@gmail.com",
-                        url = "https://github.com/AbhishekSharma-99"
-                ),
-                license = @License(
-                        name = "Apache License, Version 2.0",
-                        url = "https://www.apache.org/licenses/LICENSE-2.0.html"
-                )
-        ),
-        externalDocs = @ExternalDocumentation(
-                description = "PostPulse GitHub Repository",
-                url = "https://github.com/AbhishekSharma-99/PostPulse-Backend"
-        )
-)
 public class PostPulseApplication {
 
     public static void main(String[] args) {
